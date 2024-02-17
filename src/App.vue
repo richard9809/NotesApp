4b3beb684018a24d197d636aed656a734fee4f6c
Richard Mulu <richard.ndisya@strathmore.edu>
2024-02-17 11:46:29 +0300

    added the script logic

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,65 @@
+<script setup>
+import { ref } from "vue";
+
+const showModal = ref(false);
+const newNote = ref("");
+const errorMessage = ref("");
+const notes = ref([]);
+
+function getRandomColor() {
+  return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+}
+
+const addNote = () => {
+  if (newNote.value.length < 10) {
+    return (errorMessage.value = "Note needs to be 10 characters or more!");
+  }
+  notes.value.push({
+    id: Math.floor(Math.random() * 1000000),
+    text: newNote.value,
+    date: new Date(),
+    backgroundColor: getRandomColor(),
+  });
+
+  showModal.value = false;
+  newNote.value = "";
+  errorMessage.value = "";
+};
+</script>
+
 <template>
   <main>
-    <!-- <div class="overlay">
+    <div v-if="showModal" class="overlay">
       <div class="modal">
-        <textarea name="note" id="note" cols="30" rows="10"></textarea>
-        <button>Add Note</button>
-        <button class="close">Close</button>
+        <textarea
+          v-model.trim="newNote"
+          name="note"
+          id="note"
+          cols="30"
+          rows="10"
+        ></textarea>
+        <p v-if="errorMessage">{{ errorMessage }}</p>
+        <button @click="addNote">Add Note</button>
+        <button class="close" @click="showModal = false">Close</button>
       </div>
-    </div> -->
+    </div>
     <div class="container">
       <header>
         <h1>Notes</h1>
-        <button>+</button>
+        <button @click="showModal = true">+</button>
       </header>
       <div class="cards-container">
-        <div class="card">
+        <div
+          v-for="note in notes"
+          :key="note.id"
+          class="card"
+          :style="{ backgroundColor: note.backgroundColor }"
+        >
           <p class="main-text">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque
-            dolorem enim iure voluptatum ipsum deleniti tempora pariatur.
+            {{ note.text }}
           </p>
-          <p class="date">04/27/1982</p>
+          <p class="date">{{ note.date.toLocaleDateString("en-US") }}</p>
         </div>
-        <div class="card">
-          <p class="main-text">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque
-            dolorem enim iure voluptatum ipsum deleniti tempora pariatur.
-          </p>
-          <p class="date">04/27/1982</p>
-        </div>
-        
       </div>
     </div>
   </main>
@@ -127,5 +159,9 @@ header button {
 .modal .close {
   background-color: rgb(193, 15, 15);
   margin-top: 7px;
+}
+
+.modal p {
+  color: rgb(193, 15, 15);
 }
 </style>
